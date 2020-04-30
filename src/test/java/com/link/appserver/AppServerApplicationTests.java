dd5d6f5c9cc1ef1dc98c70c61a71cbf992db8a8d
funkyholic <funkyholic00@gmail.com>
2020-04-30 12:03:16 -0400

    initiate spring boot

---
+++ src/test/java/com/link/appserver/AppServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.link.appserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
